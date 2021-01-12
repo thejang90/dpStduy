f3cdbc5555255508c343ca2e23dbf512bbf91c74
minseok jang <thejang90@gmail.com>
2021-01-12 17:08:11 +0900

    커밋

---
+++ steps/step01.py
@@ -0,0 +1,6 @@
+class Variable:
+    '''
+    변수를 담을 클래스 생성
+    '''
+    def __init__(self,data):
+        self.data = data
---
+++ steps/step03.py
@@ -0,0 +1,22 @@
+import numpy as np
+from steps.step01 import Variable
+
+class Function:
+    def __call__(self, input):
+        x = input.data
+        y = self.forward(x)
+        output = Variable(y)
+        return output
+
+    def forward(self, x):
+        raise NotImplementedError()
+
+
+class Square(Function):
+    def forward(self, x):
+        return x ** 2
+
+class Exp(Function):
+    def forward(self, x):
+        return np.exp(x)
+
